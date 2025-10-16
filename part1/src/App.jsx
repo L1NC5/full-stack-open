4f7170054fa75916bf2fc8a82a49139aa8b20603
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-10-16 19:11:42 +0200

    1.12: anecdotes step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,65 +1,30 @@
 import { useState } from "react";
 
-const Statistics = ({ good, neutral, bad }) => {
-  if (!good && !neutral && !bad) {
-    return;
-  }
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
   return (
     <>
-      <h2>Statistics:</h2>
-      <table>
-        <tbody>
-          <StatisticLine text={"Good"} value={good} />
-          <StatisticLine text={"Neutral"} value={neutral} />
-          <StatisticLine text={"Bad"} value={bad} />
-          <StatisticLine
-            text={"Average"}
-            value={(good - bad) / (good + neutral + bad)}
-          />
-          <StatisticLine
-            text={"Positive"}
-            value={(good / (good + neutral + bad)) * 100 + "%"}
-          />
-        </tbody>
-      </table>
+      <div>{anecdotes[selected]}</div>
+      <button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      >
+        Next anectode
+      </button>
     </>
-  );
-};
-
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <th>{text}:</th>
-    <td>{value}</td>
-  </tr>
-);
-
-const Button = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
-};
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-      }}
-    >
-      <h1>Give us your feedback!</h1>
-      <h2>How was your experience at Unicafe?</h2>
-      <div style={{ display: "flex", gap: "1rem" }}>
-        <Button onClick={() => setGood(good + 1)}>Good</Button>
-        <Button onClick={() => setNeutral(neutral + 1)}>Neutral</Button>
-        <Button onClick={() => setBad(bad + 1)}>Bad</Button>
-      </div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
   );
 };
 

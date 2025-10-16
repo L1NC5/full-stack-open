4a734702d96f914cb31304f614b535d6cefc8bf0
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-10-16 18:27:54 +0200

    1.6: unicafe step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,28 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const Button = ({ onClick, children }) => {
+    return <button onClick={onClick}>{children}</button>;
   };
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+    <div style={{ display: "flex", flexDirection: "column", alignItems: 'flex-start'}}>
+      <h1>Give us your feedback!</h1>
+      <h2>How was your experience at Unicafe?</h2>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <Button onClick={() => setGood(good + 1)}>Good</Button>
+        <Button onClick={() => setNeutral(neutral + 1)}>Neutral</Button>
+        <Button onClick={() => setBad(bad + 1)}>Bad</Button>
+      </div>
+      <h2>Statistics:</h2>
+      <span>Good: {good}</span>
+      <span>Neutral: {neutral}</span>
+      <span>Bad: {bad}</span>
     </div>
   );
 };

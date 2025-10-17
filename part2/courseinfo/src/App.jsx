7d78923286ed4077f784155746af8e3c781767cb
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-10-17 23:21:29 +0200

    2.5: separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>;
-
-const Content = ({ parts }) =>
-  parts.map((part) => <Part key={part.id} part={part} />);
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-const Total = ({ parts }) => (
-  <p>
-    Number of exercises {parts.reduce((acc, obj) => acc + obj.exercises, 0)}
-  </p>
-);
-
-const Course = ({ course }) => (
-  <>
-    {course.name && <Header name={course.name} />}
-    {course.parts && course.parts.length > 0 && (
-      <>
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </>
-    )}
-  </>
-);
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
--- part2/courseinfo/src/components/Total.jsx
+++
@@ -1,7 +0,0 @@
-const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
-
-  return <p>Total exercises: {total}</p>;
-};
-
-export default Total;

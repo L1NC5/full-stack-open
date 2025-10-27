d9ed4bfa41d3fe1b76981f50c74607c393da0ea2
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-10-27 23:37:52 +0100

    2.9: the phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filterBy, setNewFilterBy] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const handleFilterBy = (event) => {
+    setNewFilterBy(event.target.value);
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -31,10 +39,20 @@ const App = () => {
     setPersons(persons.concat(newContact));
   };
 
+  const filteredPersons =
+    filterBy.trim().length > 0
+      ? persons.filter((person) =>
+          person.name.toLowerCase().includes(filterBy.toLowerCase()),
+        )
+      : persons;
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Filter by name</h2>
+      <input value={filterBy} onChange={handleFilterBy} />
       <form onSubmit={handleSubmit}>
+        <h2>Add new contact</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -46,14 +64,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div style={{display: "flex", gap: "0.5rem"}} key={person.name}>
-          <span>
-            {person.name}
-          </span>
-          <span>
-            {person.number}
-          </span>
+      {filteredPersons.map((filteredPersons) => (
+        <div style={{ display: "flex", gap: "0.5rem" }} key={filteredPersons.name}>
+          <span>{filteredPersons.name}</span>
+          <span>{filteredPersons.number}</span>
         </div>
       ))}
     </div>

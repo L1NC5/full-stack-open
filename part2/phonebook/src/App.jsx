f38e01f30234056122fa7b1ae8a388f6b95537e1
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-11-02 10:50:16 +0100

    2.11: the phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import PersonList from "./components/PersonList";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState();
   const [filterBy, setNewFilterBy] = useState("");
   const [newPerson, setNewPerson] = useState({ name: "", number: "" });
 
@@ -36,6 +32,12 @@ const App = () => {
     setPersons(persons.concat(newPerson));
   };
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -47,7 +49,7 @@ const App = () => {
         handleSubmit={handleSubmit}
       />
       <h2>Numbers</h2>
-      <PersonList persons={persons} filterBy={filterBy} />
+      {persons && <PersonList persons={persons} filterBy={filterBy} />}
     </div>
   );
 };
